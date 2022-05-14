8ae523ac532151dc837db1104a9a4b13f5b00561
Anderson Coelho <axzonexgamer@gmail.com>
2022-05-15 00:50:19 +0100

    added ability for a user to ask for a certain pokemon from the api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -10,7 +11,12 @@ struct Pokemon {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-  let resp = reqwest::get("https://pokeapi.co/api/v2/pokemon/mew")
+  let mut requestedPokemon = String::new();
+  let baseUri = "https://pokeapi.co/api/v2/pokemon/";
+  println!("which pokemon would you like to know about?");
+  io::stdin().read_line(&mut requestedPokemon).expect("failed to read");
+
+  let resp = reqwest::get(format!("{}{}",baseUri,requestedPokemon))
     .await?
     .json::<serde_json::Value>()
     .await?;
