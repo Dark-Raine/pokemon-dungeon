d6e8293a83884db269975c5e5d623b5bca869a87
Anderson Coelho <axzonexgamer@gmail.com>
2022-05-15 00:53:02 +0100

    changed variables to snake case as per the analyser

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ struct Pokemon {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-  let mut requestedPokemon = String::new();
-  let baseUri = "https://pokeapi.co/api/v2/pokemon/";
+  let mut requested_pokemon = String::new();
+  let base_uri = "https://pokeapi.co/api/v2/pokemon/";
   println!("which pokemon would you like to know about?");
-  io::stdin().read_line(&mut requestedPokemon).expect("failed to read");
+  io::stdin().read_line(&mut requested_pokemon).expect("failed to read");
 
-  let resp = reqwest::get(format!("{}{}",baseUri,requestedPokemon))
+  let resp = reqwest::get(format!("{}{}",base_uri,requested_pokemon))
     .await?
     .json::<serde_json::Value>()
     .await?;
