23ea0c75aadc9b1ea417e2220acf48d42562c023
Anderson Coelho <axzonexgamer@gmail.com>
2022-05-14 22:46:50 +0100

    added hash map to use to show json response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+  let resp = reqwest::get("https://pokeapi.co/api/v2/pokemon/ditto");
   Ok(())
 }
