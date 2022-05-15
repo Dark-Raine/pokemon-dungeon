de77d595e0b983d0ca81bc444e1639b1b902f89a
Anderson Coelho <axzonexgamer@gmail.com>
2022-05-15 20:36:09 +0100

    formatted file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
 use serde::{Deserialize, Serialize};
+use std::io;
+use std::io::Write;
 
 #[derive(Deserialize, Debug)]
 struct PokemonData {
@@ -10,7 +11,7 @@ struct PokemonData {
   stats: Vec<PokemonStat>,
 }
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize, Debug)]
 struct PokemonStat {
   #[serde(rename = "base_stat")]
   value: u32,
@@ -19,7 +20,7 @@ struct PokemonStat {
 
 #[derive(Deserialize, Debug)]
 struct PokemonStatName {
-  name: String
+  name: String,
 }
 
 #[derive(Deserialize, Debug, Serialize)]
@@ -36,9 +37,9 @@ struct Pokemon {
 impl From<PokemonData> for Pokemon {
   fn from(i: PokemonData) -> Pokemon {
     let mut stats = i.stats.iter();
-    let hp = stats.find(|&stat| stat.stat.name == "hp" ).unwrap();
-    let attack = stats.find(|&stat| stat.stat.name == "attack" ).unwrap();
-    let defense = stats.find(|&stat| stat.stat.name == "defense" ).unwrap();
+    let hp = stats.find(|&stat| stat.stat.name == "hp").unwrap();
+    let attack = stats.find(|&stat| stat.stat.name == "attack").unwrap();
+    let defense = stats.find(|&stat| stat.stat.name == "defense").unwrap();
 
     Pokemon {
       id: i.id,
@@ -57,15 +58,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
   let mut requested_pokemon = String::new();
   let base_uri = "https://pokeapi.co/api/v2/pokemon/";
   println!("which pokemon would you like to know about?");
-  io::stdin().read_line(&mut requested_pokemon).expect("failed to read");
+  io::stdin()
+    .read_line(&mut requested_pokemon)
+    .expect("failed to read");
 
-  let resp = reqwest::get(format!("{}{}",base_uri,requested_pokemon))
+  let resp = reqwest::get(format!("{}{}", base_uri, requested_pokemon))
     .await?
     .json::<serde_json::Value>()
     .await?;
-    let pokemon: PokemonData = serde_json::from_value(resp).expect("wrong Format");
-    let pokemon_parsed = Pokemon::from(pokemon);
+  let pokemon: PokemonData = serde_json::from_value(resp).expect("wrong Format");
+  let pokemon_parsed = Pokemon::from(pokemon);
+  let mut stdout = io::stdout();
+  writeln!(stdout, "{:?}", pokemon_parsed);
 
-  println!("{:?}",pokemon_parsed);
   Ok(())
 }
